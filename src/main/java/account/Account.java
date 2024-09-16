18d3ec9e8905463e6384dbc27b02845ae692816c
Xerig <74830785+Xerig@users.noreply.github.com>
2024-09-16 17:11:04 +0100

    First test step written.

--- src/main/java/account/Account.java
+++ src/main/java/account/Account.java
@@ -1,4 +1,19 @@
 package account;
 
 public class Account {
+    private final int number;
+    private final String name;
+
+    public Account(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/glue/AccountSteps.java
+++ src/test/java/glue/AccountSteps.java
@@ -1,8 +1,11 @@
 package glue;
 
 import account.Account;
-import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class AccountSteps {
 
@@ -10,7 +13,26 @@ public class AccountSteps {
 
     @Given("^Account exists for Acc No\\. \"([^\"]*)\" with Name \"([^\"]*)\"$")
     public void accountExistsForAccNoWithName(String number, String name) {
-        throw new PendingException();
+        Assert.assertEquals(Integer.parseInt(number), account.getNumber());
+        Assert.assertEquals(name, account.getName());
     }
 
+    @And("deposits are made")
+    public void depositsAreMade() {
+
+    }
+
+    @And("withdrawls are made")
+    public void withdrawlsAreMade() {
+
+    }
+
+    @When("statement is produced")
+    public void statementIsProduced() {
+
+    }
+
+    @Then("statement includes {string}")
+    public void statementIncludes(String arg0) {
+    }
 }
