978c3a7edc42096e1d59275c02a13f41c72e4b5a
Xerig <74830785+Xerig@users.noreply.github.com>
2024-09-16 15:55:46 +0100

    Working lowercase list with duplicates.

--- src/main/java/words/Words.java
+++ src/main/java/words/Words.java
@@ -1,9 +1,17 @@
 package words;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Words {
     public static List<String> getUniqueWordsFromSentence(String sentence) {
+        //Split the string on delimiter of 1 or more instances of non-words.
+        String[] splitSentence = sentence.split("\\W+");
+        ArrayList<String> lowerCaseArray = new ArrayList<>(List.of(splitSentence));
+        lowerCaseArray.replaceAll(String::toLowerCase);
+        for (String word : lowerCaseArray) {
+            System.out.println(word);
+        }
         throw new UnsupportedOperationException();
     }
 }
