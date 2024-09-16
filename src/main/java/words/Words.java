f076cbd2009341d51e84b3953a07b066ace5ddbc
Xerig <74830785+Xerig@users.noreply.github.com>
2024-09-16 16:17:15 +0100

    Tests passed, working getUniqueWordsFromSentence.

--- src/main/java/words/Words.java
+++ src/main/java/words/Words.java
@@ -1,17 +1,17 @@
 package words;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class Words {
     public static List<String> getUniqueWordsFromSentence(String sentence) {
-        //Split the string on delimiter of 1 or more instances of non-words.
+        // Split the string on delimiter of 1 or more instances of non-words.
         String[] splitSentence = sentence.split("\\W+");
         ArrayList<String> lowerCaseArray = new ArrayList<>(List.of(splitSentence));
         lowerCaseArray.replaceAll(String::toLowerCase);
-        for (String word : lowerCaseArray) {
-            System.out.println(word);
-        }
-        throw new UnsupportedOperationException();
+        // Convert list to a data structure that does not allow duplicates,
+        // then convert and return the final list.
+        return new ArrayList<>(new LinkedHashSet<>(lowerCaseArray));
     }
 }
