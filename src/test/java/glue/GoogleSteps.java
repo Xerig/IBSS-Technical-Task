5f70b4062ca316e8f1a109080adedfd298268e33
Xerig <74830785+Xerig@users.noreply.github.com>
2024-09-17 18:07:51 +0100

    Test steps for Scenario 1 are completed. Shows 'About' page and checks for mission statement text.

--- src/test/java/glue/GoogleSteps.java
+++ src/test/java/glue/GoogleSteps.java
@@ -1,8 +1,14 @@
 package glue;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 
 public class GoogleSteps {
 
@@ -18,5 +24,22 @@ public class GoogleSteps {
         } catch (NoSuchElementException ignored) {
         }
     }
+
+    @When("About page is shown")
+    public void aboutPageIsShown() {
+        try {
+            W.get().driver.findElement(By.linkText("About")).click();
+        } catch (NoSuchElementException ignored) {
+        }
+    }
+
+    @Then("page displays {string}")
+    public void pageDisplays(String textToFind) {
+        // Get all text displayed on the page under the body tag
+        String allBodyText = W.get().driver.findElement(By.tagName("body")).getText();
+        // Check that the page contains the text in the Scenario
+        Assert.assertTrue(allBodyText.contains(textToFind));
+    }
+
 
 }
